79b619b8ad850c457b23e8dc9747315b12f31f86
0107anocchilupo <0107anocchilupo@PC9774.domepfc.net>
2017-12-07 21:12:41 +0100

    Isuzu commit 7/12/2017 21:12

---
+++ Demo/Isuzu/contact.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Isuzu Ute Australia</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="style.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <header>
+            <h1>Isuzu Ute Australia</h1>
+            <ul>
+                <li><a class="link" href="index.php"> Home</a></li>
+                <li><a class="link" href="index.php">D-MAX</a></li>
+                <li><a class="link" href="index.php">MU-X</a></li>
+                <li><a class="link" href="contact.php">Contact</a></li>
+            </ul>
+        </header>
+        
+        <section id="main">
+            <h2 id="titre">Contact Us</h2>
+            <p>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2519.0675016962655!2d4.432743215797469!3d50.8484342664888!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c3dc9f69538d39%3A0xfa5333abbc80aef2!2sChauss%C3%A9e+de+Roodebeek+35%2C+1200+Woluwe-Saint-Lambert%2C+Belgique!5e0!3m2!1sfr!2sfr!4v1507824942782"
+                        frameborder="0" allowfullscreen></iframe>
+            </p>
+
+            <h3>Pacific Isuzu Ute</h3>
+            <div class="flex">
+                <article>
+                    <section>
+                        <h4 class="titre">Adress</h4>
+                        <p>
+                            721 Niclin Way<br>
+                            Currimundi, QLD, 4551
+                        </p>
+                    </section>
+                    <section>
+                        <h4 class="titre">Contact</h4>
+                        <p>
+                            <strong>Phone: </strong>(07) 5438 488<br>
+                            <strong>Fax: </strong>(07) 5438 4899<br>
+                            <strong>Email: </strong><a id="hyperLink" href="contactForm.php">enquiries@pacificusuzu.com</a>
+                        </p>
+                    </section>
+                </article>
+
+                <article>
+                    <h4 class="titre">Sales Opening Hours</h4>
+                    <p>
+                        Mon: 8:00am - 5:30pm<br>
+                        Tue: 8:00am - 5:30pm<br>
+                        Wed: 8:00am - 5:30pm<br>
+                        Thu: 8:00am - 5:30pm<br>
+                        Fri: 8:00am - 5:30pm<br>
+                        Sat: 8:00am - 4:00pm<br>
+                        Sun: Closed
+                    </p>
+                </article>
+
+                <article>
+                    <h4 class="titre">Available This Dealership</h4>
+                    <ul id="liens">
+                        <li class="liste">Sales</li>
+                        <li class="liste">Services</li>
+                        <li class="liste">Spare Parts</li>
+                    </ul>
+                </article>
+            </div>
+        </section>
+    </body>
+</html>
